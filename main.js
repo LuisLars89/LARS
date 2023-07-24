44c32e7c8a7e10d751f9c84b2ca47affea28a8d3
LuisLars89 <tacuboo@hotmail.com>
2023-07-24 02:41:12 -0600

    Updates

--- main.js
+++ main.js
@@ -20,8 +20,8 @@ let buttonInfo          = document.querySelector('.add-to-cart-button')
 let productosEnCarrito  = []
 let productoACarrito    = []
 
-//countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
-totalCarrito.innerText = '$10,890.00'
+countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
+totalCarrito.innerText = '$0.00'
 
 emailMenu.addEventListener('click', toggleMenu);
 iconMenuMobile.addEventListener('click', toggleMenuMobile);
@@ -81,90 +81,140 @@ function closeModal(){
 
 let productList = []
 productList.push({
-    name:"ERICK",
-    price:"10,000",
-    image:"./icons/Pick.jpeg",
-    desc: 'Leagues Cup'
+    name:"house",
+    price:180,
+    image:"https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Casa en fraccionamiento privado en excelente zona residencial con caseta de vigilancia y guardia, a 15 minutos del centro de la ciudad y muy cerca de centros comerciales y escuelas.'
 })
 productList.push({
-    name:"CORONADO",
-    price:"10,000",
-    image:"./icons/Pick.jpeg",
-    desc: 'Leagues Cup'
+    name:"house model 2",
+    price:120,
+    image: "https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Renta: $ 30,000\nMetros de construcción: 297m2\nMetros de Terreno: 250m2'
 })
 productList.push({
-    name:"GERA",
-    price:"10,000",
-    image:"./icons/Pick.jpeg",
-    desc: 'Leagues Cup'
+    name:"house model 3",
+    price:200,
+    image:"https://images.pexels.com/photos/280229/pexels-photo-280229.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Oportunidad de casa de dos plantas y con excelente ubicación, cerca de avenidas principales de la ciudad a tres minutos de la Jose María Iglesias, así como de centros comerciales, escuelas y centros de esparcimiento.'
 })
 productList.push({
-    name:"JL",
-    price:"10,000",
-    image:"./icons/Pick.jpeg",
-    desc: 'Leagues Cup'
+    name:"house model 4",
+    price:895,
+    image:"https://images.pexels.com/photos/206172/pexels-photo-206172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Casa en Renta recién Remodelada en Paseo de las Misiones'
 })
 productList.push({
-    name:"MAGUA",
-    price:"10,000",
-    image:"./icons/Pick.jpeg",
-    desc: 'Leagues Cup'
+    name:"house model 5",
+    price:1500,
+    image:"https://images.pexels.com/photos/1438832/pexels-photo-1438832.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Rento casa amueblada en Fraccionamiento Quintas del Sol, muy cerca de la Av. Ortíz Mena, terreno amplio, cochera con capacidad de hasta 4 carros, sala, comedor, cocina, lavandería, medio baño, recamara completa, todo en planta baja'
 })
 productList.push({
-    name:"COMANDANTE",
-    price:"10,000",
-    image:"./icons/Pick.jpeg",
-    desc: 'Leagues Cup'
+    name:"house model 6",
+    price:100,
+    image:"https://images.pexels.com/photos/2581922/pexels-photo-2581922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Propiedad en venta o renta ubicada sobre Av. Mirador'
 })
 productList.push({
-    name:"CHOCO",
-    price:"10,000",
-    image:"./icons/Pick.jpeg",
-    desc: 'Leagues Cup'
+    name:"house model 7",
+    price:3620,
+    image:"https://images.pexels.com/photos/53610/large-home-residential-house-architecture-53610.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    desc: 'Propiedad en venta o renta ubicada sobre Av. Mirador'
 })
 
-function renderListProduct(productList) {
+function renderListProduct(productList){
     for (const product of productList) {
-        let divproductCard = document.createElement('div');
-        let imgproductcard = document.createElement('img');
-        let divproductinfo = document.createElement('div');
-        let divdivproductinfo = document.createElement('div');
-        let pprice = document.createElement('p');
-        let pname = document.createElement('p');
-        let figureproductinfo = document.createElement('figure');
+        let divproductCard = document.createElement('div')
+        let imgproductcard = document.createElement('img')
+        let divproductinfo = document.createElement('div')
+        let divdivproductinfo = document.createElement('div')
+        let pprice = document.createElement('p')
+        let pname = document.createElement('p')
+        let figureproductinfo = document.createElement('figure')
+        let imgproductinfo = document.createElement('img')
+        divproductCard.classList.add('product-card')
+        imgproductcard.setAttribute('src', product.image)
+        divproductinfo.classList.add('product-info')
+        pprice.innerText ='$' + product.price
+        pname.innerText = product.name
+        imgproductinfo.setAttribute('src', './icons/bt_add_to_cart.svg')
+        
+        figureproductinfo.appendChild(imgproductinfo)
+        divdivproductinfo.appendChild(pprice)
+        divdivproductinfo.appendChild(pname)
 
-        // Creamos el enlace y lo envolvemos alrededor de la imagen imgproductinfo
-        let anchor = document.createElement('a');
-        anchor.setAttribute('href', 'https://sports.caliente.mx/es_MX/Leagues-Cup/2023-07-22/CF-Montreal-vs-Pumas-UNAM'); // Reemplaza 'URL_DEL_ENLACE' con la URL deseada
-        let imgproductinfo = document.createElement('img');
-        imgproductinfo.setAttribute('src', './icons/caliente2.svg');
-        anchor.appendChild(imgproductinfo);
+        divproductinfo.appendChild(divdivproductinfo)
+        divproductinfo.appendChild(figureproductinfo)
 
-        divproductCard.classList.add('product-card');
-        imgproductcard.setAttribute('src', product.image);
-        divproductinfo.classList.add('product-info');
-        pprice.innerText = '$' + product.price;
-        pname.innerText = product.name;
+        divproductCard.appendChild(imgproductcard)
+        divproductCard.appendChild(divproductinfo)
 
-        figureproductinfo.appendChild(anchor); // Usamos el enlace con la imagen dentro de la figura
-        divdivproductinfo.appendChild(pprice);
-        divdivproductinfo.appendChild(pname);
+        divcardscontainer.appendChild(divproductCard)
 
-        divproductinfo.appendChild(divdivproductinfo);
-        divproductinfo.appendChild(figureproductinfo);
-
-        divproductCard.appendChild(imgproductcard);
-        divproductCard.appendChild(divproductinfo);
-
-        divcardscontainer.appendChild(divproductCard);
-
-        imgproductcard.addEventListener('click', function() {
-            mostrarInfoProduct(product.image, product.price, product.name, product.desc);
-            openProductDetail();
+        imgproductcard.addEventListener('click', function(){
+            mostrarInfoProduct(product.image, product.price, product.name, product.desc)
+            openProductDetail()
         });
+
+
+        imgproductinfo.addEventListener('click', function(){
+            let productoAgregado = []
+            productoAgregado.push({
+                name: product.name,
+                price: product.price,
+                image: product.image
+            })
+            agregarCarrito(productoAgregado)
+        })
     }
 }
 
+/* Funcion para agregar productos al carrito */
+function agregarCarrito(producto){
+    if(verificarProducto(producto[0].name)){
+        productoACarrito= []
+        return modal.style.display = 'grid'
+    }
+
+    /* Creacion de elemtentos HTML */
+    let divShoppingCart = document.createElement('div')
+    let figureShoppingCart = document.createElement('figure')
+    let imgfigureShoppingCart = document.createElement('img')
+    let pnameShoppingCart = document.createElement('p')
+    let ppriceShoppingCart = document.createElement('p')
+    let imgcloseShoppingCart = document.createElement('img')
+
+    /* Se agrega contenido a cada uno de los elementos */
+    divShoppingCart.classList.add('shopping-cart')
+    imgfigureShoppingCart.setAttribute('src', producto[0].image)
+    pnameShoppingCart.innerText = producto[0].name
+    ppriceShoppingCart.innerText ='$' + producto[0].price
+    imgcloseShoppingCart.setAttribute('src','./icons/icon_close.png')
+    imgcloseShoppingCart.classList.add('removeList')
+
+    /* Se insertan los documentos */
+    figureShoppingCart.appendChild(imgfigureShoppingCart)
+    divShoppingCart.appendChild(figureShoppingCart)
+    divShoppingCart.appendChild(pnameShoppingCart)
+    divShoppingCart.appendChild(ppriceShoppingCart)
+    divShoppingCart.appendChild(imgcloseShoppingCart)
+    myordercontentContainer.appendChild (divShoppingCart)
+
+    /* Se agregan la cantidad de productos ademas de la suma de precios */
+    countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
+    totalCarrito.innerText = '$' + (Number(totalCarrito.innerText.substring(1)) + Number(producto[0].price))
+    productosEnCarrito.push(producto[0].name)
+    
+    /* Funcion para quitar productos del carrito */
+    imgcloseShoppingCart.addEventListener('click', function(){
+        divShoppingCart.remove()
+        countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
+        totalCarrito.innerText = '$' + (Number(totalCarrito.innerText.substring(1)) - Number(producto[0].price))
+        productosEnCarrito.splice(productosEnCarrito.indexOf(producto[0].name),1)
+    }) 
+
+}
 
 function mostrarInfoProduct(imagen, precio, nombre, descripcion){
     productImageInfor.setAttribute('src', imagen)
