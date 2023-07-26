35bc5a85139d12f9640d0350a17887f56e730cd8
LuisLars89 <tacuboo@hotmail.com>
2023-07-25 22:06:50 -0600

    Correccion

--- src/main.js
+++ src/main.js
@@ -21,6 +21,8 @@ let productosEnCarrito  = []
 let productoACarrito    = []
 
 let enlace;
+let nombre;
+let precio;
 
 countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
 totalCarrito.innerText = '$0.00'
@@ -102,13 +104,13 @@ productList.push({
 })
 productList.push({
     name:"F1-75 Monza, 100 aniversario",
-    price:"10,000",
+    price:"960.79",
     image:"./Mcia/Ferrari.png",
     desc: 'ERRARI 2017 Monza Circuit 100th Anniversary'
 })
 productList.push({
     name:"Funda para Movil",
-    price:"10,000",
+    price:"2000",
     image:"./Mcia/Funda.png",
     desc: 'iPhone 14, 13, 12, 11 Pro Max, Cup'
 })
@@ -187,7 +189,9 @@ function agregarCarrito(producto){
     enlace=producto[0].image
     
     pnameShoppingCart.innerText = producto[0].name
+    nombre=producto[0].name
     ppriceShoppingCart.innerText ='$' + producto[0].price
+    precio = producto[0].price
     imgcloseShoppingCart.setAttribute('src','./icons/icon_close.png')
     imgcloseShoppingCart.classList.add('removeList')
 
@@ -201,14 +205,19 @@ function agregarCarrito(producto){
 
     /* Se agregan la cantidad de productos ademas de la suma de precios */
     countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
-    totalCarrito.innerText = '$' + (Number(totalCarrito.innerText.substring(1)) + Number(producto[0].price))
+    let totalCarritoValue = parseFloat(totalCarrito.innerText.substring(1));
+    let productPrice = parseFloat(producto[0].price);
+    let newTotal = totalCarritoValue + productPrice;
+    totalCarrito.innerText = '$' + newTotal.toFixed(2); // toFixed(2) para mostrar el total con 2 decimales
+    //totalCarrito.innerText = '$' + (Number(totalCarrito.innerText.substring(1)) + Number(producto[0].price))
     productosEnCarrito.push(producto[0].name)
     
     /* Funcion para quitar productos del carrito */
     imgcloseShoppingCart.addEventListener('click', function(){
         divShoppingCart.remove()
         countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
-        totalCarrito.innerText = '$' + (Number(totalCarrito.innerText.substring(1)) - Number(producto[0].price))
+        
+        totalCarrito.innerText = '$' + (parseFloat(totalCarrito.innerText.substring(1)) - parseFloat(producto[0].price))
         productosEnCarrito.splice(productosEnCarrito.indexOf(producto[0].name),1)
     }) 
 
@@ -264,20 +273,35 @@ renderListProduct(productList);
     var img = new Image();
     img.src = enlace; // 'enlace' should be the URL of the image you want to print
   
+    var logo = new Image();
+    logo.src = "./logos/BigLogo.png" // 'enlace' should be the URL of the image you want to print
+
     // Wait for the image to load
     img.onload = function () {
       // Set the position and size of the image on the page
-      var x = 50; // X coordinate of the image
+      var x = 20; // X coordinate of the image
       var y = 50; // Y coordinate of the image
-      var width = 100; // Width of the image
-      var height = 100; // Height of the image
-  
+      var width = 80; // Width of the image
+      var height = 80; // Height of the image
+        
+      var xLogo = 20; // X coordinate of the logo (adjust this to position the logo)
+      var yLogo = 20; // Y coordinate of the logo (adjust this to position the logo)
+      
+        
+
+
       // Draw the image on the PDF document
       pdf.addImage(img, 'JPEG', x, y, width, height);
+      pdf.addImage(logo, 'JPEG', xLogo, yLogo);
   
       // Write the name to the page
-      pdf.text("Your text here", 100, 100);
-  
+      pdf.text(nombre, 100, 80);
+      pdf.text("$"+precio, 100, 100);
+      pdf.text("CUENTA: ", 20, 150);
+      pdf.text("PAGO OXXO: ", 20, 160);  
+
+      pdf.text("Reenviar Comprobante de pago a : ", 20, 180);  
+
       // Save the PDF document
       pdf.save("my-pdf.pdf");
     };
